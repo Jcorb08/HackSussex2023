15bff036f36179dc37eb5aa4f8bef90db426c57f
Joe C <52496811+Jcorb08@users.noreply.github.com>
2023-02-19 02:53:44 +0000

    halfway with registerform

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,40 @@
-// Webserver Code
-// Keep the bot alive!
-var http = require('http');
-const server = http.createServer();
-server.listen(8080);
+jQuery(function() {
+    // …
+    
+    
+    function registerUser(data){
 
-
-alert("test");
+        $.ajax({
+            url: "https://api.sharksgambit.tech/register/new_user/",
+            type: "POST",
+            crossDomain: true,
+            data: {
+                username: data,
+                CSRF: getCSRFTokenValue()
+            },
+            dataType: "json",
+            success: function (response) {
+                var resp = JSON.parse(response)
+                alert(resp.status);
+            },
+            error: function (xhr, status) {
+                alert("error");
+            }
+        });
+            
+        $.post("https://api.sharksgambit.tech/register/new_user/",
+        {
+            username: data[0]
+        },
+        function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    }
+    
+    
+    $('#register').on('submit',function(event){
+        event.preventDefault();
+        console.log('haha!',$(this).serializeArray())
+        registerUser($(this).serializeArray()[0])
+    });
+  });
