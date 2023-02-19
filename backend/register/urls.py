310d1c7d02250df96f7578aca1a2bf26a3dc9cfe
Aniels <you@example.com>
2023-02-19 05:02:57 +0000

    rename new user url

--- backend/register/urls.py
+++ backend/register/urls.py
@@ -6,11 +6,11 @@ from .views import register
 
 urlpatterns = [
     path('', register.index, name='index'),
-    path('new_user/', register.first_time_register, name = "first_time_register"),
+    path('post_new_user/', register.first_time_register, name = "first_time_register"),
     path('reset_registered_user/', register.reset_registered_user, name = 'reset_registered_user'),
     path('initial_standard_buff_table/', view_buff.initial_standard_buff_table, name = 'initial_standard_buff_table'),
     path('initial_standard_equipments_table/', view_equirement.initial_standard_equipments_table, name = 'initial_standard_equipments_table'),
     path('get_all_standard_equipments/', view_equirement.get_all_standard_equipments, name = 'get_all_standard_equipments'),
-    path('')
+    path('post_for_equipment_card', view_equirement.post_for_equipment_card, name = 'post_for_equipment_card')
 
 ]
