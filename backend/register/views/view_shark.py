9ea4c578a835b90c8dbb4d6f40d454653fda2c9f
Aniels <you@example.com>
2023-02-19 06:03:34 +0000

    post_for_attack_shark

--- backend/register/views/view_shark.py
+++ backend/register/views/view_shark.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
+from ..models.equipment import standard_equipment
 from ..models.shark import shark
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def post_for_attack_shark(request):
+    if request.method == "POST":
+        attack_value = standard_equipment.objects.filter(equipment_name = request.POST.get('equipment_name')).first().attack
+        attacked_shark = shark.objects.filter(id = request.POST.get('shark_id')).first()
+        attacked_shark.hp -= attack_value
+        attacked_shark.save()
+        
+    return HttpResponse(attacked_shark.hp)
+
 
 
 def initial_standard_equipments_table(request):
-    
     pass
