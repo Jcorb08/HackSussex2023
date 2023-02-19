0af3b2aef29fd15e05b6a7b0938b971eae27a40b
Skrofo <123328336+Skrofo@users.noreply.github.com>
2023-02-19 02:45:06 +0000

    Item

--- frontend/EquiptmentSelector.js
+++ frontend/EquiptmentSelector.js
@@ -1,7 +1,7 @@
 alert("String")
 let equiptment = document.querySelectorAll(".equiptment")
-for (let index = 0; index < equiptments.length; index++) {
-        equiptments[index].addEventListener("click",(e) =>
+for (let index = 0; index < equiptment.length; index++) {
+        equiptment[index].addEventListener("click",(e) =>
         {
                 let id = e.target.getAttribute("id")
                 console.log(id)
@@ -11,11 +11,11 @@ for (let index = 0; index < equiptments.length; index++) {
 
 function selectequiptment(i)
 {
-        for (let index = 0; index < equiptments.length; index++) {
-                equiptments[index].classList.remove("active")
+        for (let index = 0; index < equiptment.length; index++) {
+                equiptment[index].classList.remove("active")
         }
-        equiptments[i].classList.add("active")
+        equiptment[i].classList.add("active")
 }
                 console.log(id)
 
-console.log(equiptments)
+console.log(equiptment)
