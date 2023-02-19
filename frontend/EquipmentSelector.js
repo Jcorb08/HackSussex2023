38b73ca310a1695f6e627c1c94d427a8971be30b
Skrofo <123328336+Skrofo@users.noreply.github.com>
2023-02-19 02:49:55 +0000

    item 3

---
+++ frontend/EquipmentSelector.js
@@ -0,0 +1,21 @@
+alert("String")
+let equipment = document.querySelectorAll(".equipment")
+for (let index = 0; index < equipment.length; index++) {
+        equipment[index].addEventListener("click",(e) =>
+        {
+                let id = e.target.getAttribute("id")
+                console.log(id)
+                selectequipment(id) 
+        })
+}
+
+function selectequipment(i)
+{
+        for (let index = 0; index < equipment.length; index++) {
+                equipment[index].classList.remove("active")
+        }
+        equipment[i].classList.add("active")
+}
+                console.log(id)
+
+console.log(equipment)
